d7297332759d51f799bc6fd087e0de095daab759
kuschelljane <schellaneam@gmail.com>
2023-04-20 10:16:05 +0800

    fix image

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -22,7 +22,7 @@ const Featured = () => {
   return (
     <div className={styles.container}>
       <div className={styles.arrowContainer} style={{ left: 0 }} onClick={()=>handleArrow("l")}>
-        <Image src="/img/arrowl.png" alt="" layout="fill" objectFit="contain"/>
+        <Image src={require('/img/arrowl.png')} alt="" layout="fill" objectFit="contain"/>
       </div>
       <div className={styles.wrapper} style={{transform:`translateX(${-100*index}vw)`}}>
         {images.map((img, i) => (
@@ -32,7 +32,7 @@ const Featured = () => {
         ))}
       </div>
       <div className={styles.arrowContainer} style={{ right: 0 }} onClick={()=>handleArrow("r")}>
-        <Image src="/img/arrowr.png" layout="fill" alt="" objectFit="contain"/>
+      <Image src={require('/img/arrowr.png')} alt="" layout="fill" objectFit="contain"/>
       </div>
     </div>
   );
